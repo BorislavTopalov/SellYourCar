03cd76c29dbef4e68dc9f35c1d6dc489807f28ce
BorislavTopalov <borislawtopalow@gmail.com>
2022-10-15 11:44:05 +0300

    router commit

--- sell-your-car/src/App.js
+++ sell-your-car/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className='header'>
+
+      </header>
+      <div className='pageContent'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='*' element={<div className='home'>"HomePage"</div>} />
+            <Route path='login' element={<div className='login'>"LoginPage"</div>} />
+            <Route path='register' element={<div className='register'>"RegisterPage"</div>} />
+            <Route path='profile' element={<div className='profile'>"ProfilePage"</div>} />
+            <Route path='addNew' element={<div className='addNew'>"AddNewPage"</div>} />
+            <Route path='addPictures' element={<div className='addPictures'>"AddPictures"</div>} />
+            <Route path='publication' element={<div className='publication'>"Publication"</div>} />
+            <Route path='showingNewAd' element={<div className='showingTheNew'>"ShowingTheNewAd"</div>} />
+            <Route path='detailSearching' element={<div className='detailSearch'>"DetailSearching"</div>} />
+            <Route path='allResults' element={<div className='allResults'>"AllResults"</div>} />
+            <Route path='showTheChosenAd' element={<div className='showTheChosenAd'>"ShowTheChosenAd"</div>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+
+      <footer className='footer'>
+
+      </footer>
     </div>
   );
 }
